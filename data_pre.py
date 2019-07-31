938bb70c1953a934636282ddf3fa5f2beb83b89d
Dupi16 <44947979+Dupi16@users.noreply.github.com>
2019-07-31 15:24:20 +0700

    Add files via upload

--- data_pre.py
+++ data_pre.py
@@ -14,9 +14,7 @@ files = []
 dirwalk("./", files, "*.utt")
 
 #Xu li
-label_li = []
-sentence_li = []
-person_li = []
+
 #Bo trg hop "%"
 li_1 = ['ba','qy', 'x', 't1', 't3', '^t', '^c', 'oo', 'o', 'qw', 'qo', 'qrr', 'qr', 'qh', '^d',
 '^g', 'ad', 'co', 'cc', 'fp', 'fc', 'fx', 'fe', 'fo', 'ft', 'fw', 'fa', 'aap', 'aa', 'am', 
@@ -32,130 +30,152 @@ print(len(li_1))
 print(len(li_2))
 
 
-now = []
-for item in files:
-    file = open(item, 'r')
-    s = file.read().splitlines()
-    now += s
-
-
-
-
-for sentence in now:
-        sentence = sentence.strip()
-
-        if (sentence.find('A.') != -1 or sentence.find('B.') != -1) and sentence.find("utt") != -1:
-
-        #label
-                if sentence.startswith('%') == True:
-                        if sentence.find('-/') != -1 or sentence.find('- /') != -1:
-                                label_li.append('abandoned')
-                        else:
-                                label_li.append('uninterpretable')
-                else:
-                        for i in range(61):
-                                a = sentence.startswith(li_1[i])
-                                if a == True:
-                                        label_li.append(li_2[i])
-                                        break
-                                else:
-                                        continue
-        else:
-                continue
-#A,B
-for sentence in now:
-        if sentence.find('A.') != -1 and sentence.find("utt") != -1:
-                person_li.append('A')
-        elif sentence.find('B.') != -1 and sentence.find("utt") != -1:
-                person_li.append('B')
-        else:
-                continue
-        
-#content
-for sentence in now:
-        sentence = sentence.strip().rstrip('/')
-        
-        if (sentence.find('A.') != -1 or sentence.find('B.') != -1) and sentence.find("utt") != -1:
-                m = sentence.split(':', 1)
-                sentence_li += m
-        else:
-                continue
-print(len(sentence_li))
-
-for i in range(447210, -2, -2):
-        del sentence_li[i]
-
-#Xu li content
-
-
-for n in range(300):
-        sentence = sentence_li[n].strip()
-        while sentence.find('{') != -1 and sentence.find('}') != -1:
-                i = sentence.index('{')
-                j = sentence.index('}')
-                sentence = sentence[:i] + sentence[(j+2):]
-
-        sentence_li[n] = sentence
-        
-for n in range(300):
-        sentence = sentence_li[n]
-        while sentence.find('<') != -1 and sentence.find('>') != -1:
-                i = sentence.index('<')
-                j = sentence.index('>')
-                sentence = sentence[:i] + sentence[(j+2):]
-
-        sentence_li[n] = sentence.strip()
-
-
-        
-for n in range(300):
-        if sentence_li[n].endswith('+'):
-                person_li[n] = person_li[n+1]
-                label_li[n] = ''
-                sentence_li[n+2] = sentence_li[n] + ' ' + sentence_li[n+2]
-                sentence_li[n] = ''
-        else:
-                continue
-for n in range(300):
-        if sentence_li[n].endswith('--'):
-                person_li[n] = person_li[n+1]
-                label_li[n] = ''
-                sentence_li[n+2] = sentence_li[n].rstrip('--').strip() + ' ' + sentence_li[n+2].lstrip('--').strip()
-                sentence_li[n] = ''
-        else:
-                continue
-
-for n in range(300):
-        sentence = sentence_li[n]
-        while sentence.find(']') != -1 and sentence.find('[') != -1 and sentence.find('+') != -1:
-                i = sentence.index('[')
-                j = sentence.index('+')
-                t = sentence.index(']')
-                sentence = sentence[:(i+1)] + sentence[(j+2):]
-        sentence = sentence.replace('[', '').replace(']', '')
-        sentence = sentence.strip('-').replace('  ', ' ')
-        
-
-for n in range(50):
-        sentence_li[n] = sentence_li[n].strip()
-        while person_li[n+1] == person_li[n]:
-                sentence_li[n+1] = sentence_li[n].strip() + ' ' + sentence_li[n+1].strip()
-                label_li[n] = ''
-                sentence_li[n] = ''
-                person_li[n] = ''
-                
-
-
-print(sentence_li[0:13])
-
-
-#Write
 file_w = open("./con_swbd1.py", 'wb')
 d = '| {:^6} | {:^30} | {:<10} |\n'.format('Person', 'Tag', 'Sentence')
 file_w.write(d.encode())
-for i in range(300):
-        c = '| {:^6} | {:^30} | {:<10} |\n'.format(person_li[i], label_li[i], sentence_li[i])
-        file_w.write(c.encode())
+
+
+for item in files:
+        label_li = []
+        sentence_li = []
+        person_li = []
+        now = []
+        file = open(item, 'r')
+        now = file.read().splitlines()
+        
+
+
+
+
+        for sentence in now:
+                sentence = sentence.strip()
+
+                if (sentence.find('A.') != -1 or sentence.find('B.') != -1) and sentence.find("utt") != -1:
+
+                #label
+                        if sentence.startswith('%') == True:
+                                if sentence.find('-/') != -1 or sentence.find('- /') != -1:
+                                        label_li.append('abandoned')
+                                else:
+                                        label_li.append('uninterpretable')
+                        else:
+                                for i in range(61):
+                                        a = sentence.startswith(li_1[i])
+                                        if a == True:
+                                                label_li.append(li_2[i])
+                                                break
+                                        else:
+                                                continue
+                else:
+                        continue
+        #A,B
+        for sentence in now:
+                if sentence.find('A.') != -1 and sentence.find("utt") != -1:
+                        person_li.append('A')
+                elif sentence.find('B.') != -1 and sentence.find("utt") != -1:
+                        person_li.append('B')
+                else:
+                        continue
+                
+        #content
+        for sentence in now:
+                sentence = sentence.strip().rstrip('/')
+                
+                if (sentence.find('A.') != -1 or sentence.find('B.') != -1) and sentence.find("utt") != -1:
+                        m = sentence.split(':', 1)
+                        sentence_li += m
+                else:
+                        continue
+        print(len(sentence_li))
+
+        for i in range(len(sentence_li)-2, -2, -2):
+                del sentence_li[i]
+
+        #Xu li content
+
+
+        for n in range(len(sentence_li)):
+                sentence = sentence_li[n].strip()
+                while sentence.find('{') != -1 and sentence.find('}') != -1:
+                        i = sentence.index('{')
+                        j = sentence.index('}')
+                        sentence = sentence[:i] + sentence[(j+2):]
+
+                sentence_li[n] = sentence
+                
+        for n in range(len(sentence_li)):
+                sentence = sentence_li[n]
+                while sentence.find('<') != -1 and sentence.find('>') != -1:
+                        i = sentence.index('<')
+                        j = sentence.index('>')
+                        sentence = sentence[:i].strip() +' ' +sentence[(j+2):].strip()
+
+                sentence_li[n] = sentence.strip()
+
+
+                
+        for n in range(len(sentence_li)):
+                if sentence_li[n].endswith('+'):
+                        person_li[n] = person_li[n+1]
+                        label_li[n] = ''
+                        sentence_li[n+2] = sentence_li[n] + ' ' + sentence_li[n+2]
+                        sentence_li[n] = ''
+                else:
+                        continue
+        for n in range(len(sentence_li)):
+                if sentence_li[n].endswith('--'):
+                        person_li[n] = person_li[n+1]
+                        label_li[n] = ''
+                        sentence_li[n+2] = sentence_li[n].rstrip('--').strip() + ' ' + sentence_li[n+2].lstrip('--').strip()
+                        sentence_li[n] = ''
+                else:
+                        continue
+
+        for n in range(len(sentence_li)):
+                sentence = sentence_li[n]
+                while sentence.find(']') != -1 and sentence.find('[') != -1 and sentence.find('+') != -1:
+                        i = sentence.index('[')
+                        j = sentence.index('+')
+                        t = sentence.index(']')
+                        sentence = sentence[:(i+1)].strip() + ' ' + sentence[(j+2):].strip()
+                sentence = sentence.replace('[', ' ').replace(']', ' ')
+                sentence_li[n] = sentence.strip('-').replace('  ', ' ')
+        
+        for i in range(len(sentence_li)-1, -1, -1):
+                if sentence_li[i] == '':
+                        del sentence_li[i]
+                        del label_li[i]
+                        del person_li[i]
+                else:
+                        continue
+
+        for n in range(len(sentence_li)):
+                sentence_li[n] = sentence_li[n].strip()
+                while n+1 < len(sentence_li) and person_li[n+1] == person_li[n]:
+                        sentence_li[n+1] = sentence_li[n].strip() + ' ' + sentence_li[n+1].strip()
+                        label_li[n] = ''
+                        sentence_li[n] = ''
+                        person_li[n] = ''
+                sentence_li[n] = sentence_li[n].replace('#', ' ').replace('  ', ' ').strip()
+
+
+        for i in range(len(sentence_li)-1, -1, -1):
+                if sentence_li[i] == '':
+                        del sentence_li[i]
+                        del label_li[i]
+                        del person_li[i]
+                else:
+                        continue
+
+
+#Write
+        
+        
+        for i in range(len(sentence_li)):
+                c = '| {:^6} | {:^30} | {:<10} |\n'.format(person_li[i], label_li[i], sentence_li[i])
+                file_w.write(c.encode())
+                
+        file_w.write(('------------------------------------\n').encode())
 
 file_w.close()
 
